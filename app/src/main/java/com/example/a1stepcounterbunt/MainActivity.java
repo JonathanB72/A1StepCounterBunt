85053d2129647306f54177d4a490c295fd30d47d
Jonathan <f.jonathan.bunt@gmail.com>
2020-02-17 15:33:10 -0600

    Step Detection basic algorithm, proximity sensor to hide start and stop buttons if in pocket

--- app/src/main/java/com/example/a1stepcounterbunt/MainActivity.java
+++ app/src/main/java/com/example/a1stepcounterbunt/MainActivity.java
@@ -22,11 +22,11 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
     private SensorManager mSensorManager;
     private Sensor mAccelerometer;
-    private Sensor sensors;
+    private Sensor mProximity;
 
     private TextView XYZText, ShakinText, CountText, thsdOutput;
     private EditText thsdInput;
-    private Button StartButton, StopButton;
+    private Button StartButton, StopButton, ProxButton;
 
     private long CurrentTime;
     private long LastTime;
@@ -39,11 +39,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private float[] ZSlidingWindow = new float[50];
     private float[] SumSlidingWindow = new float[50];
     private boolean Shakin = false;
+    private boolean ProximityFlag = false;
     private float thsdNum = 0; //THSD!
+    private float distance = 0; //PROX!
     private int Count = 0;
 
     private String XYZDummyText = "{0, 0, 0}";
     private String thsdDummyText = "thsd: 0"; //THSD!
+    private String shakeDummyText = "not shaking";
+    private String ProximityDummyText = "Turn Prox Feature On";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -53,9 +57,17 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         // Accelerometer data
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        List<Sensor> sensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+        mProximity = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
 
-        //update thsd on editText
+        if (mAccelerometer != null) {
+            //heck yeah our accelerometer is working
+            mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_GAME);
+        }
+        else{
+            //fail
+        }
+
+        //BUTTONS
         StartButton.setOnClickListener(
                 new View.OnClickListener() {
                     public void onClick(View view) {
@@ -75,13 +87,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 }
         );
 
-        if (mAccelerometer != null) {
-            //heck yeah our accelerometer is working
-            mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_GAME);
-        }
-        else{
-            //fail
-        }
+        ProxButton.setOnClickListener(
+                new View.OnClickListener(){
+                    public void onClick(View view){
+                        ProximityFlag = !ProximityFlag;
+                    }
+                }
+        );
+
+
     }
 
     public void initializeAllTheStuff(){
@@ -95,6 +109,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         //Buttons yay
         StartButton = (Button) findViewById(R.id.StartButton);
         StopButton = (Button)findViewById(R.id.StopButton);
+        ProxButton = (Button)findViewById(R.id.ProxButton);
 
     }
 
@@ -102,6 +117,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onResume() {
         super.onResume();
         mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_GAME);
+        mSensorManager.registerListener(this,mProximity, SensorManager.SENSOR_DELAY_GAME);
     }
     //onPause() unregister the accelerometer for stop listening the events
     protected void onPause() {
@@ -116,22 +132,41 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onSensorChanged(SensorEvent event){
-        XValue = event.values[0];
-        YValue = event.values[1];
-        ZValue = event.values[2];
-        SumValue = (XValue+YValue+ZValue);
-        CurrentTime = System.currentTimeMillis();
-        //10 milliseconds
-        if(CurrentTime - LastTime > 10){
-            addToWindow(); //SW
-            LastTime = CurrentTime;
-        }
+        if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            XValue = event.values[0];
+            YValue = event.values[1];
+            ZValue = event.values[2];
+            SumValue = (XValue + YValue + ZValue);
+            CurrentTime = System.currentTimeMillis();
+            //10 milliseconds
+            if (CurrentTime - LastTime > 10) {
+                addToWindow(); //SW
+                LastTime = CurrentTime;
+            }
 
-        isShaking();
-        //if stopped shaking for more than 3 seconds, Shakin = false
-        if(CurrentTime - ShakinTime > 3000)
-            Shakin = false;
-        updateText();
+            isShaking();
+            //if stopped shaking for more than 3 seconds, Shakin = false
+            if (CurrentTime - ShakinTime > 3000)
+                Shakin = false;
+            updateText();
+        }
+        //PROXIMITY SENSOR EXTRA CREDIT
+        if(event.sensor.getType() == Sensor.TYPE_PROXIMITY){
+            if(ProximityFlag){
+                //This can either be 0 if close or 8 if far
+                distance = event.values[0];
+                if(distance == 0){ // hide buttons if in pocket
+                    StartButton.setVisibility(View.GONE);
+                    StopButton.setVisibility(View.GONE);
+                }else{
+                    StartButton.setVisibility(View.VISIBLE);
+                    StopButton.setVisibility(View.VISIBLE);
+                }
+            }else{
+                StartButton.setVisibility(View.VISIBLE);
+                StopButton.setVisibility(View.VISIBLE);
+            }
+        }
     }
 
     public void addToWindow(){ //SW
@@ -144,7 +179,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         XSlidingWindow[XSlidingWindow.length-1]=Math.round(XValue);
         YSlidingWindow[YSlidingWindow.length-1]=Math.round(YValue);
         ZSlidingWindow[ZSlidingWindow.length-1]=Math.round(ZValue);
-        SumSlidingWindow[SumSlidingWindow.length-1]= SumValue;
+        SumSlidingWindow[SumSlidingWindow.length-1]= Math.round(SumValue);
     }
 
     public void resetWindows(){
@@ -158,16 +193,33 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public void isShaking(){
         //check to see if the phone is shaking
         //Thsd Crossing
-        for(int i = 1; i < XSlidingWindow.length; i++){
-            //if the last number is less than the thsd and the next one is greater than or equal
-            if((XSlidingWindow[i-1] > thsdNum) && ((XSlidingWindow[i] < thsdNum) || (XSlidingWindow[i] == thsdNum))){
-                Count++;
-                //reset the sliding window as soon as this is true
-                resetWindows();
-                Shakin = true;
-                ShakinTime = CurrentTime;
+
+        //if z crossing 0 first and foremost, easiest step detection method I found
+
+        if(ZSlidingWindow[0] < 0 && ZSlidingWindow[49] >0){
+            Count++;
+            resetWindows();
+            Shakin = true;
+            ShakinTime = CurrentTime;
+        } else {
+        //else check the sliding window and if the last number is less than the thsd
+        // and the next one is greater than it then detect a shake/step
+
+            for (int i = 1; i < XSlidingWindow.length; i++) {
+                if ((SumSlidingWindow[i - 1] > thsdNum) && (SumSlidingWindow[i] < thsdNum)) {
+                    Count++;
+                    //reset the sliding window as soon as soon as detected
+                    resetWindows();
+                    Shakin = true;
+                    ShakinTime = CurrentTime;
+                }
             }
         }
+    }
+
+    public void stepCounter(){
+
+
     }
 
     public void updateText(){
@@ -175,10 +227,23 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         XYZDummyText = "{" + Math.round(XValue*10.0)/10.0 + ", " + Math.round(YValue*10.0)/10.0 + ", " + Math.round(ZValue*10.0)/10.0 + "}";
         thsdDummyText = "thsd: " + thsdNum;
 
+        if(Shakin) {
+            shakeDummyText = "Shaking";
+        }else {
+            shakeDummyText = "Not Shaking";
+        }
+
+        if(ProximityFlag) {
+            ProximityDummyText = "Turn Prox Feature Off";
+        }else {
+            ProximityDummyText = "Turn Prox Feature On";
+        }
+
         XYZText.setText(XYZDummyText);
         thsdOutput.setText(thsdDummyText);
-        ShakinText.setText(Boolean.toString(Shakin));
+        ShakinText.setText(shakeDummyText);
         CountText.setText(Integer.toString(Count));
+        ProxButton.setText(ProximityDummyText);
     }
 
 
